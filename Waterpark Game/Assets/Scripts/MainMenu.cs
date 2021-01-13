c34cbd683d1c23388f7922b5f808ddce82c159ee
Mysbjorn <48379212+Mysbjorn@users.noreply.github.com>
2021-01-13 21:14:51 +0100

    Main menu scene

---
+++ Waterpark Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public string firstLevel, levelSelect;
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(firstLevel);
+    }
+
+    //public void Continue()
+    //{
+    //    SceneManager.LoadScene(levelSelect);
+    //}
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
